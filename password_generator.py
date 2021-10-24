714c7d47f766c647a7b79b9d2c6f9b212104e48b
Christopher Snell <chris.snell2019@gmail.com>
2021-10-24 10:05:08 -0700

    Added numbers list and special characters

--- password_generator.py
+++ password_generator.py
@@ -3,6 +3,8 @@ import random
 
 lowercase = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v','w', 'x', 'y', 'z']
 uppercase = []
+nums_list = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+special_letters = ['!', '@', '#', '$', "%"]
 
 for lower in lowercase:
     uppercase.append(lower.upper())
