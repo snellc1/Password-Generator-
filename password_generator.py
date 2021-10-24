5c35a64dd5c63cce40c4f20182ffba23002d3e32
Christopher Snell <chris.snell2019@gmail.com>
2021-10-24 11:28:38 -0700

    Password Generator

--- password_generator.py
+++ password_generator.py
@@ -6,6 +6,7 @@ uppercase = []
 nums_list = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
 special_letters = ['!', '@', '#', '$', "%"]
 
+# Making uppercase list
 for lower in lowercase:
     uppercase.append(lower.upper())
 
