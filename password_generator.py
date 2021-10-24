147c494193dd3718324808bdd217ce5a9554c2fc
Christopher Snell <chris.snell2019@gmail.com>
2021-10-24 10:36:20 -0700

    Password generator with no conditions.

--- password_generator.py
+++ password_generator.py
@@ -1,7 +1,6 @@
 # Create a password that is 8 - 16 long. 
 import random as rm 
 
-
 lowercase = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v','w', 'x', 'y', 'z']
 uppercase = [ ]
 nums_list = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
