6540bdd9c9ad10e7ccc24a803f772a8cfd6b1ea2
Christopher Snell <chris.snell2019@gmail.com>
2021-10-24 10:01:18 -0700

    Add lowercase and uppercase list

--- password_generator.py
+++ password_generator.py
@@ -1,2 +1,8 @@
 # Create a password that is 8 - 16 long. 
 import random
+
+lowercase = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v','w', 'x', 'y', 'z']
+uppercase = []
+
+for lower in lowercase:
+    uppercase.append(lower.upper())
